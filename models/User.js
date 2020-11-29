c86d5232c0e98be51a09ec0dbc0b7a6db7e1bcaf
augedoo <augedoo@gmail.com>
2020-11-29 13:46:37 +0000

    Added validation message for duplicate email

--- models/User.js
+++ models/User.js
@@ -12,7 +12,7 @@ const UserSchema = new mongoose.Schema(
     email: {
       type: String,
       required: [true, 'Please add an email'],
-      unique: true,
+      unique: [true, 'A user with that email already exist'],
       match: [
         /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
         'Please add a valid email',
